161ce9ece48193f511db987608f786f525cc2305
Ben Paton <benpaton@Bens-MacBook-Pro.local>
2014-08-06 20:55:10 +0100

    Update to html 5 and e-mail to be html

--- anonEmail.php
+++ anonEmail.php
@@ -1,21 +1,36 @@
 <?php
+
 	//get the vars from post
     $to = $_POST["to"];
 	$from = $_POST["from"];
 	$subject = $_POST["subject"];
 	$message = $_POST["message"];
 	$send = $_POST["send"];
+   
     //send the message
 	if ($send == 'send' && $message != 'message') {
-    	mail($to, $subject, $message, "FROM: {$from}");
+		
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers .= 'FROM: '.$from . "\r\n";
+		
+		$HTMLmessage = '
+			<html>
+			<head>
+			  <title>'.$subject.'</title>
+			</head>
+			<body>'.$message.'</body>
+			</html>
+		';
+		
+    	mail($to, $subject, $HTMLmessage, $headers);
 	}	
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
 <title>Anonymous E-mailer</title>
-<style>
+<style type="text/css">
 body {
 	font-family:Arial, Helvetica, sans-serif;
 }
@@ -34,15 +49,14 @@ textarea {
 }
 </style>
 </head>
-
 <body>
 <?php if ($send != 'send') { ?>
-<form action="" method="post">
+<form method="post">
 	<ul>
-        <li><input type="text" name="to" id="to" value="to" onclick="if (this.value=='to') { this.value='' }" onblur="if (this.value=='') { this.value='to' }" /></li>
-        <li><input type="text" name="from" id="from" value="from" onclick="if (this.value=='from') { this.value='' }" onblur="if (this.value=='') { this.value='from' }" /></li>
-        <li><input type="text" name="subject" id="subject" value="subject" onclick="if (this.value=='subject') { this.value='' }" onblur="if (this.value=='') { this.value='subject' }" /></li>
-        <li><textarea name="message" id="message" onclick="if (this.value=='message') { this.value='' }" onblur="if (this.value=='') { this.value='message' }">message</textarea></li>
+        <li><input type="text" name="to" placeholder="to" /></li>
+        <li><input type="text" name="from" placeholder="from" /></li>
+        <li><input type="text" name="subject" placeholder="subject" /></li>
+        <li><textarea name="message" placehodler="message"></textarea></li>
         <li><input type="hidden" name="send" id="send" value="send" /><input type="submit" name="submit" value="submit" /></li>
     </ul>
 </form>
